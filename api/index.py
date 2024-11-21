d2f14809ec91bbbcd813d85f6e4bc591bf03c12e
unknown <you@example.com>
2024-11-21 20:42:01 +0200

    2323

--- api/index.py
+++ api/index.py
@@ -1,53 +1,11 @@
-from flask import Flask, request, send_file
-from PIL import Image, ImageDraw, ImageFont
-import requests
-import io
+from flask import Flask
 
 app = Flask(__name__)
 
-# Получение города по IP
-def get_city_by_ip(ip):
-    try:
-        response = requests.get(f"https://ipinfo.io/{ip}/json",verify=False)
-        data = response.json()
-        print(data["city"])
-        return data["city"]
-    except Exception:
-        return "Unknown City"
+@app.route('/')
+def home():
+    return 'Hello, World!'
 
-# Генерация изображения
-def create_image_with_text(text):
-    # Создаем белое изображение размером 500x500
-    img = Image.new("RGB", (500, 500), color="white")
-    draw = ImageDraw.Draw(img)
-    
-    # Настройка шрифта
-    try:
-        font = ImageFont.truetype("arial.ttf", 36)  # Убедитесь, что шрифт установлен
-    except IOError:
-        font = ImageFont.load_default()
-
-    # Вычисление размеров текста с использованием textbbox
-    text_bbox = draw.textbbox((0, 0), text, font=font)
-    text_width = text_bbox[2] - text_bbox[0]
-    text_height = text_bbox[3] - text_bbox[1]
-    
-    # Центрирование текста
-    position = ((500 - text_width) // 2, (500 - text_height) // 2)
-    draw.text(position, text, fill="black", font=font)
-    return img
-
-
-@app.route("/", methods=["GET"])
-def index():
-    # Получаем IP клиента
-    client_ip = request.remote_addr
-    
-    city = get_city_by_ip(client_ip)
-    img = create_image_with_text(city)
-    
-    # Сохраняем изображение в буфер для отправки
-    buf = io.BytesIO()
-    img.save(buf, format="PNG")
-    buf.seek(0)
-    return send_file(buf, mimetype="image/png")
+@app.route('/about')
+def about():
+    return 'About'
